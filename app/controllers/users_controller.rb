0bcc490add0c0ffe07bf142f38f00709f4681027
“tuna <davmassoud@gmail.com>
2015-07-31 02:37:17 +0200

    dude i am so drunk i don't know what i am adding right now, but i know it's something #ballmercurve

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,12 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
-  def create
+	def show
+		@user = User.find(params[:id])
+	end
+	
+
+ 	def create
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
