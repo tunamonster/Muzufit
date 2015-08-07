c8e9482256e7caaa81c3103b096bc052da70a153
David Massoud <davmassoud@gmail.com>
2015-08-07 11:22:13 +0200

    finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,15 +13,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
-  test "valid signup should redirect to user page" do
+  test "valid signup information" do
     get signup_path
-    assert_difference 'User.count' do 
-       post users_path, user: { name:  "foo",
-                               email: "foo@foo.foo",
-                               password:              "foofoo",
-                               password_confirmation: "foofoo" }
+    assert_difference 'User.count' do
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
   end
-  assert_template 'users/show' #not sure if this is the corrext syntax to indicate the newly created user 
-end
 
 end
