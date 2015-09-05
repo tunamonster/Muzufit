2e3cd2c89c4b5e55a8842629028427378fe29576
fearthedear <linuskinzel@gmail.com>
2015-09-05 18:20:05 +0200

    nothing

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ActiveRecord::Base
               foreign_key: "user_id",
               dependent: :destroy
 
-	  attr_accessor :remember_token, :activation_token
-    before_save { self.email = email.downcase }
-    before_create :create_activation_digest
+    attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -66,6 +66,10 @@ class User < ActiveRecord::Base
    UserMailer.account_activation(self).deliver_now
  end
   private
+    def downcase_email
+      self. email = email.downcase
+    end
+    
     def create_activation_digest
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
