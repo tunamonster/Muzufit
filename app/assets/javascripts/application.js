72beb8f1d0e32ce5965625f27b7f550d28f50be1
fearthedear <linuskinzel@gmail.com>
2015-09-10 20:33:03 +0200

    optimisations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,7 @@
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
   });
+
+  $(document).ready(function(){
+  $('.parallax').parallax();
+  });
