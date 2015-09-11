62f235c07ba65a95c11f0f9f6aef5ddecc52e834
fearthedear <linuskinzel@gmail.com>
2015-09-12 01:51:47 +0200

    setting up pw reset and heroku production mailing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905191523) do
+ActiveRecord::Schema.define(version: 20150911215449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 20150905191523) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_foreign_key "cpostings", "users"
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -15,6 +15,8 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
   test "password_reset" do
+    user = users(:customer)
+    user.reset_token = User.new_token
     mail = UserMailer.password_reset
     assert_equal "Password reset", mail.subject
     assert_equal ["to@example.org"], mail.to
